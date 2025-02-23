3abef1bd081a2c2e3ad46b044fa2c7a630e80a82
Anish Palakurthi <anish.palakurthi@gmail.com>
2025-02-23 02:34:24 +0000

    mongodb stable again

--- scripts/setup-benchmarks/install-ycsb.sh
+++ scripts/setup-benchmarks/install-ycsb.sh
@@ -13,24 +13,12 @@ fi
 # Setup
 mkdir $YCSB_BENCHMARK_DIR
 
-# Clone
 pushd $YCSB_BENCHMARK_DIR
-git clone https://github.com/brianfrankcooper/YCSB.git
-pushd YCSB
-git remote add delete-fork https://github.com/minhokang242/AzureCosmos.git
-git fetch delete-fork
-git checkout delete-fork/users/minhokang/azurecosmos_coreworkload_delete
-git rebase master
-
-# Build
-mvn -pl site.ycsb:mongodb-binding -am clean package
-mvn -pl site.ycsb:redis-binding -am clean package
-mvn -pl site.ycsb:memcached-binding -am clean package
+curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar xfvz ycsb-0.17.0.tar.gz
+mv ycsb-0.17.0 YCSB
 
 popd
-popd
-
-pwd
 
 # Copy contents of ycsb_runner.py to bin/ycsb
 cp scripts/setup-benchmarks/ycsb_runner.py $YCSB_BENCHMARK_DIR/YCSB/bin/ycsb
