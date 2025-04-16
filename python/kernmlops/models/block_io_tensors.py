92564cd68b8c9d01a456ce946ad1b33143695dc5
tewaro <adityaatewari@gmail.com>
2025-04-15 20:49:21 -0500

    chore: add yet another thing to extract data

--- python/kernmlops/models/block_io_tensors.py
+++ python/kernmlops/models/block_io_tensors.py
@@ -2,9 +2,30 @@ import random
 from pathlib import Path
 from typing import Any, Mapping, Protocol
 
-import featurewiz
 import polars as pl
 import torch
+
+import argparse
+from enum import Enum
+
+# Setup operation
+class Mode(Enum):
+    WHIZ = "whiz"
+    MINT = "mint"
+    INVE = "investigate"
+
+def mode_type(value: str) -> Mode:
+    try:
+        return Mode(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Invalid mode: {value}. Choose from {[m.value for m in Mode]}")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--mode", type=mode_type, default=Mode.WHIZ, help=f"Select mode. Options: {[m.value for m in Mode]}")
+
+args = parser.parse_args()
+
 
 req_opf = {
     0: "Read",
@@ -41,6 +62,61 @@ def _explode_flags(flags: int) -> list[int]:
     exploded_flags.append(1 if flags & REQ_NOWAIT else 0)
     return exploded_flags # length 9
 
+def convert_df(df: pl.DataFrame) -> pl.DataFrame:
+    df = df.sort(["device", "ts_uptime_us"]).with_columns([
+            (pl.col("block_io_flags") & REQ_OP_MASK).replace_strict(req_opf).alias("op"),
+            pl.when((pl.col("block_io_flags") & REQ_SYNC > 0)).then(True).otherwise(False).alias("sync_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_META > 0)).then(True).otherwise(False).alias("metadata_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_FUA > 0)).then(True).otherwise(False).alias("fua_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_PRIO > 0)).then(True).otherwise(False).alias("priority_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_NOMERGE > 0)).then(True).otherwise(False).alias("nomerge_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_IDLE > 0)).then(True).otherwise(False).alias("idle_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_RAHEAD > 0)).then(True).otherwise(False).alias("readahead_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_BACKGROUND > 0)).then(True).otherwise(False).alias("background_flag"),
+            pl.when((pl.col("block_io_flags") & REQ_NOWAIT > 0)).then(True).otherwise(False).alias("nowait_flag"),
+            pl.col("sector").shift(1, fill_value=0).alias("sector_offset"),
+            pl.col("ts_uptime_us").shift(1, fill_value=0).alias("ts_offset"),
+        ]).with_columns([
+            (pl.col("ts_uptime_us") - pl.col("ts_offset")).alias("ts_uptime_us"),
+            (pl.col("sector") - pl.col("sector_offset")).alias("sector_offset"),
+        ]).select([
+            "cpu",
+            "device",
+            "sector",
+            "sector_offset",
+            "segments",
+            "block_io_bytes",
+            "ts_uptime_us",
+            "queue_length_segment_ios",
+            "queue_length_4k_ios",
+            "block_latency_us",
+            "op",
+            "sync_flag",
+            "metadata_flag",
+            "fua_flag",
+            "priority_flag",
+            "nomerge_flag",
+            "idle_flag",
+            "readahead_flag",
+            "background_flag",
+            "nowait_flag",
+        ])
+    df = df.filter(pl.col("op") == "Read")
+    for i in ["sync_flag", "metadata_flag", "fua_flag", "priority_flag", "nomerge_flag",
+              "idle_flag", "readahead_flag", "background_flag", "nowait_flag"]:
+        new_df = df.filter(pl.col(i))
+        if new_df.height == 0:
+            df = df.drop(i)
+
+    concat_df = pl.DataFrame()
+    for i in [271581184, 271581185, 271581186]:
+        out_df = df.filter(pl.col("device") == i)
+        for j in out_df.columns:
+            for k in range(1, 8):
+                out_df = out_df.with_columns(pl.col(j).shift(i).alias(f"{j}_lag_{k}"))
+
+        concat_df = pl.concat([concat_df, out_df])
+    return concat_df
 
 class RowTransformer(Protocol):
 
@@ -52,7 +128,6 @@ class RowTransformer(Protocol):
 
     @classmethod
     def convert_row(cls, row: Mapping[str, Any], present_ts_us: int) -> list[float]: ...
-
 
 class RowFilter(Protocol):
 
@@ -109,7 +184,6 @@ class SegmentSpartanTransformer(RowTransformer):
         data.append(row["queue_length_segment_ios"])
         data.append(row["block_latency_us"] if row["ts_uptime_us"] + row["block_latency_us"] < present_ts_us else 0)
         return data
-
 
 class SegmentMinimalFlagsTransformer(RowTransformer):
 
@@ -172,6 +246,25 @@ class SegmentSpartanAllFlagsTransformer(RowTransformer):
         data.append(row["block_latency_us"] if row["ts_uptime_us"] + row["block_latency_us"] < present_ts_us else 0)
         return data
 
+class SegmentSpartanFwhizTransformer(RowTransformer):
+
+    @classmethod
+    def name(cls) -> str:
+        return "segment_spartan_fwhiz"
+
+    @classmethod
+    def feature_length(cls) -> int:
+        return 5
+
+    @classmethod
+    def convert_row(cls, row: Mapping[str, Any], present_ts_us: int) -> list[float]:
+        data = list[float]()
+        data.append(row["queue_length_segment_ios"])
+        data.append(row["queue_length_4k_ios"])
+        data.append(1 if row["block_io_flags"] & REQ_SYNC > 0 else 0)
+        data.append(1 if row["block_io_flags"] & REQ_NOMERGE > 0 else 0)
+        data.append(row["block_latency_us"] if row["ts_uptime_us"] + row["block_latency_us"] < present_ts_us else 0)
+        return data
 
 class P95Prediction(RowPrediction):
 
@@ -320,11 +413,11 @@ class BlockIOTransformer(DatasetTransformer):
 
     @classmethod
     def row_transformer(cls) -> RowTransformer:
-        return SegmentSpartanTransformer()
+        return SegmentSpartanFwhizTransformer()
 
     @classmethod
     def row_filters(cls) -> list[RowFilter]:
-        return [NoopFilter()] #, EvenFastReadFilter(), ReadsOnlyFilter(), EvenReadsOnlyFilter()]
+        return [NoopFilter(), EvenFastReadFilter(), ReadsOnlyFilter(), EvenReadsOnlyFilter()]
 
     @classmethod
     def row_prediction_transformers(cls) -> list[RowPrediction]:
@@ -332,7 +425,7 @@ class BlockIOTransformer(DatasetTransformer):
 
     @classmethod
     def num_rows(cls) -> int:
-        return 7
+        return 1
 
     def convert_and_save_parquet(self, data_df: pl.DataFrame, *, tensor_type: str, tensor_dir: str) -> str:
         root_out_dir = Path(tensor_dir)
@@ -346,6 +439,7 @@ class BlockIOTransformer(DatasetTransformer):
             "block_io_flags",
             "queue_length_segment_ios",
             "queue_length_4k_ios",
+            "block_io_latency_us",
             "block_latency_us",
             "collection_id",
         ]).rows(named=True)
@@ -422,47 +516,33 @@ test_df = pl.read_parquet("data/rainsong_test_curated/block_io/*.parquet").filte
     ])
 )
 
+if args.mode is Mode.WHIZ:
+    import featurewiz
+    train_df_blah = convert_df(train_df)
+    test_df_blah = convert_df(test_df)
+    train_df_blah = train_df_blah.filter(pl.col("op") == "Read")
+    raw_data = train_df_blah.to_pandas()
+    outputs = featurewiz.featurewiz(dataname=raw_data, target="block_latency_us",
+                                    corr_limit=1, verbose=2, test_data="", feature_engg="", category_encoders="")
 
-raw_data = train_df.sort(["device", "ts_uptime_us"]).with_columns([
-    (pl.col("block_io_flags") & REQ_OP_MASK).alias("req_op_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_SYNC > 0)).then(1).otherwise(0).alias("sync_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_META > 0)).then(1).otherwise(0).alias("metadata_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_FUA > 0)).then(1).otherwise(0).alias("fua_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_PRIO > 0)).then(1).otherwise(0).alias("priority_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_NOMERGE > 0)).then(1).otherwise(0).alias("nomerge_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_IDLE > 0)).then(1).otherwise(0).alias("idle_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_RAHEAD > 0)).then(1).otherwise(0).alias("readahead_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_BACKGROUND > 0)).then(1).otherwise(0).alias("background_flag"),
-    pl.when((pl.col("block_io_flags") & REQ_NOWAIT > 0)).then(1).otherwise(0).alias("nowait_flag"),
-    pl.col("sector").shift(1, fill_value=0).alias("sector_offset"),
-    pl.col("ts_uptime_us").shift(1, fill_value=0).alias("ts_offset"),
-]).with_columns([
-    (pl.col("ts_uptime_us") - pl.col("ts_offset")).alias("ts_uptime_us"),
-    (pl.col("sector") - pl.col("sector_offset")).alias("sector_offset"),
-]).select([
-    "cpu",
-    "device",
-    "sector",
-    "sector_offset",
-    "segments",
-    "block_io_bytes",
-    "ts_uptime_us",
-    "queue_length_segment_ios",
-    "queue_length_4k_ios",
-    "block_latency_us",
-    "req_op_flag",
-    "sync_flag",
-    "metadata_flag",
-    "fua_flag",
-    "priority_flag",
-    "nomerge_flag",
-    "idle_flag",
-    "readahead_flag",
-    "background_flag",
-    "nowait_flag",
-]).to_pandas()
-print(raw_data)
-featurewiz.featurewiz(dataname=raw_data, target="block_latency_us", corr_limit=.7, verbose=2, test_data="", feature_engg="", category_encoders="")
+    outputs[0].append("block_latency_us")
+    to_select = outputs[0]
 
-#BlockIOTransformer().convert_and_save_parquet(train_df, tensor_type="train", tensor_dir=tensor_dir)
-#BlockIOTransformer().convert_and_save_parquet(test_df, tensor_type="test", tensor_dir=tensor_dir)
+    test_df = test_df_blah.select(to_select)
+    train_df = train_df_blah.select(to_select)
+    test_df.write_parquet(tensor_dir / Path("test.fwhiz.parquet"))
+    train_df.write_parquet(tensor_dir / Path("train.fwhiz.parquet"))
+
+if args.mode is Mode.MINT:
+    BlockIOTransformer().convert_and_save_parquet(train_df, tensor_type="train", tensor_dir=tensor_dir)
+    BlockIOTransformer().convert_and_save_parquet(test_df, tensor_type="test", tensor_dir=tensor_dir)
+
+if args.mode is Mode.INVE:
+    train_df = convert_df(train_df)
+    print(train_df.filter((pl.col("op") == "Read") & (pl.col("idle_flag"))))
+    for i in ["sync_flag", "metadata_flag", "fua_flag", "priority_flag", "nomerge_flag",
+              "idle_flag", "readahead_flag", "background_flag", "nowait_flag"]:
+        new_df = train_df.filter(pl.col(i))
+        if new_df.height != 0:
+            print(f"{i}: {new_df.height}")
+            print(new_df.select(pl.col("op").unique()))
