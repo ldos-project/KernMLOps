8889826faaf4f6e7c548960911e5bc6e54a01ced
tewaro <adityaatewari@gmail.com>
2025-04-17 19:12:04 -0500

    So close

--- module/policies/bloat/bloat.ebpf.c
+++ module/policies/bloat/bloat.ebpf.c
@@ -5,50 +5,159 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-const char redis_comm[] = "redis-server";
-const char pr_comm[] = "pr";
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, 1);
+  __type(key, u32);
+  __type(value, u32);
+} singular_cache_array SEC(".maps");
 
-typedef struct singular_cache {
-  u32 valid;
-  u32 tgid;
-} singular_cache_t;
+const u32 zero_index = 0;
+
+#define CHECK_TGID_RETURN(x)                                                  \
+  do {                                                                        \
+    u32* val = (u32*)bpf_map_lookup_elem(&singular_cache_array, &zero_index); \
+    if (!val)                                                                 \
+      return 0;                                                               \
+    u32 useful = *val;                                                        \
+    if (useful == 0 || useful == (x))                                         \
+      return 0;                                                               \
+  } while (0)
+
+typedef struct rss_data {
+  u64 ts;
+  u64 count;
+  u64 valid;
+} rss_data_t;
 
 struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
   __uint(max_entries, 1);
   __type(key, u32);
-  __type(value, singular_cache_t);
+  __type(value, u64);
   __uint(map_flags, BPF_F_MMAPABLE);
-} singular_cache_array SEC(".maps");
+} rss_head_idx SEC(".maps");
 
-bool is_expected_comm(const char* comm) {
-  return (!bpf_strncmp(redis_comm, sizeof(redis_comm), comm)) ||
-         (!bpf_strncmp(pr_comm, sizeof(pr_comm), comm));
+#define BUFFER_ENTRIES 256
+
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, BUFFER_ENTRIES);
+  __type(key, u32);
+  __type(value, rss_data_t);
+  __uint(map_flags, BPF_F_MMAPABLE);
+} rss_buffer SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 32768);
+  __type(key, u32);
+  __type(value, u64);
+} rss_hash SEC(".maps");
+
+SEC("raw_tracepoint/rss_stat")
+int raw_trace_rss_stat(struct bpf_raw_tracepoint_args* ctx) {
+  u64 ts = bpf_ktime_get_ns();
+  u32 pid = bpf_get_current_pid_tgid();
+  struct mm_struct* mm = (struct mm_struct*)ctx->args[0];
+  struct task_struct* task;
+  if (!bpf_probe_read_kernel(&task, sizeof(task), &mm->owner))
+    return 0;
+  u32 other_tgid = 0;
+  if (bpf_probe_read_kernel(&other_tgid, sizeof(u32), &task->tgid))
+    return 0;
+
+  CHECK_TGID_RETURN(other_tgid);
+
+  bpf_map_update_elem(&rss_hash, &pid, &ts, BPF_ANY);
+  return 0;
 }
 
-const int zero_index = 0;
-
-bool check_singular_array(u32 tgid) {
-  singular_cache_t* val = bpf_map_lookup_elem(&singular_cache_array, &zero_index);
-  if (val) {
-    if (__atomic_fetch(&val->valid, __ATOMIC_ACQUIRE)) {
-      return val->tgid == tgid;
-    } else {
-      char comm[13];
-      bpf_get_current_comm(comm, 13);
-      if (is_expected_comm(comm)) {
-        val->tgid = tgid;
-        __atomic_set(&val->valid, 1, __ATOMIC_RELEASE);
-        return true;
-      }
-      return false;
-    }
-  }
+static rss_data_t* rss_reserve_buffer_space(u32 base_offset, u32 header_offset, u32 buff_size) {
+  u64* head_idx_ptr = bpf_map_lookup_elem(&rss_head_idx, &header_offset);
+  if (!head_idx_ptr)
+    return NULL;
+  u32 index = (u32)__atomic_fetch_add(head_idx_ptr, 1, __ATOMIC_ACQUIRE);
+  index = (u32)(index % buff_size) + base_offset;
+  rss_data_t* data = bpf_map_lookup_elem(&rss_buffer, &index);
+  if (!data)
+    return data;
+  u64 blah;
+  u64 val = 0;
+  __atomic_exchange(&data->valid, &val, &blah, __ATOMIC_ACQUIRE);
+  return data;
 }
 
-typedef struct rss_data {
-  u64 ts;
-  u64 convert_data;
-} rss_data_t;
+static void submit_data(rss_data_t* data) {
+  u64 blah;
+  u64 val = 1;
+  __atomic_exchange(&data->valid, &val, &blah, __ATOMIC_RELEASE);
+}
 
 SEC("tracepoint/kmem/rss_stat")
+int trace_rss_stat(struct trace_event_raw_rss_stat* args) {
+  u32 pid = bpf_get_current_pid_tgid();
+  u64 ts;
+  u64* tsptr = (u64*)bpf_map_lookup_elem(&rss_hash, &pid);
+  if (tsptr == NULL)
+    return 0;
+  u64 count = args->size >> 12;
+  rss_data_t* data = rss_reserve_buffer_space(0, 0, BUFFER_ENTRIES);
+  if (!data)
+    return 0;
+  data->ts = *tsptr;
+  data->count = count;
+  submit_data(data);
+  return 0;
+}
+
+#define CPUS 64
+
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, 1);
+  __type(key, u32);
+  __type(value, rss_data_t);
+  __uint(map_flags, BPF_F_MMAPABLE);
+} dtlb_header SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, BUFFER_ENTRIES);
+  __type(key, u32);
+  __type(value, rss_data_t);
+  __uint(map_flags, BPF_F_MMAPABLE);
+} dtlb_buffer SEC(".maps");
+
+static rss_data_t* perf_reserve_buffer_space(u32 base_offset, u32 header_offset, u32 buff_size) {
+  u64* head_idx_ptr = bpf_map_lookup_elem(&dtlb_header, &header_offset);
+  if (!head_idx_ptr)
+    return NULL;
+  u32 index = (u32)__atomic_fetch_add(head_idx_ptr, 1, __ATOMIC_ACQUIRE);
+  index = (u32)(index % buff_size) + base_offset;
+  rss_data_t* data = bpf_map_lookup_elem(&dtlb_buffer, &index);
+  if (!data)
+    return data;
+  u64 blah;
+  u64 val = 0;
+  __atomic_exchange(&data->valid, &val, &blah, __ATOMIC_ACQUIRE);
+  return data;
+}
+
+SEC("perf_event")
+int dtlb_miss_rate(struct bpf_perf_event_data* ctx) {
+  u32 pid = bpf_get_current_pid_tgid();
+  CHECK_TGID_RETURN(pid);
+  struct bpf_perf_event_value value_buf;
+  u64 ts = bpf_ktime_get_ns();
+  if (bpf_perf_prog_read_value(ctx, (void*)&value_buf, sizeof(struct bpf_perf_event_value))) {
+    return 0;
+  }
+  rss_data_t* data = perf_reserve_buffer_space(0, 0, BUFFER_ENTRIES);
+  if (!data)
+    return 0;
+  data->ts = ts;
+  data->count = value_buf.counter;
+  submit_data(data);
+  return 0;
+}
---
+++ module/policies/bloat/bloat_policy_deployer.cpp
@@ -0,0 +1,75 @@
+#include "../../fstore/fstore.h"
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <iostream>
+#include <linux/bpf.h>
+#include <linux/perf_event.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <tuple>
+#include <unistd.h>
+#include <vector>
+
+const char* probes_file = "bloat_probe.o";
+
+#define RETURN_ERRNO(x)                                                            \
+  if (!(x)) {                                                                      \
+    int err_errno = errno;                                                         \
+    fprintf(stderr, "%s:%d: errno:%s\n", __FILE__, __LINE__, strerror(err_errno)); \
+    return -err_errno;                                                             \
+  }
+
+int perf_event_open(struct perf_event_attr* hw_event, pid_t pid, int cpu, int group_fd,
+                    unsigned long flags) {
+  return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+}
+
+// take pid as input
+
+int main(int argc, char** argv) {
+  int pid = std::atoi(argv[0]);
+  struct bpf_object* obj;
+  obj = bpf_object__open_file(probes_file, NULL);
+  RETURN_ERRNO(obj);
+
+  RETURN_ERRNO(bpf_object__load(obj) == 0);
+
+  struct bpf_program* prog = bpf_object__find_program_by_name(obj, "raw_trace_rss_stat");
+  int prog_fd = bpf_program__fd(prog);
+
+  // Attach to tracepoint
+  struct bpf_link* link = bpf_program__attach_raw_tracepoint(prog, "rss_stat");
+  RETURN_ERRNO(link);
+
+  prog = bpf_object__find_program_by_name(obj, "trace_rss_stat");
+  prog_fd = bpf_program__fd(prog);
+
+  // Attach to tracepoint
+  link = bpf_program__attach_tracepoint(prog, "kmem", "rss_stat");
+  RETURN_ERRNO(link);
+
+  prog = bpf_object__find_program_by_name(obj, "dtlb_miss_rate");
+  prog_fd = bpf_program__fd(prog);
+
+  // Create perf setup
+  struct perf_event_attr attr = {};
+  attr.type = PERF_TYPE_HW_CACHE;
+  attr.config = PERF_COUNT_HW_CACHE_L1D | (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+  attr.size = sizeof(attr);
+  attr.disabled = 1;
+  attr.sample_freq = 1000;
+
+  int perf_fd = perf_event_open(&attr, pid, -1, -1, 0);
+
+  RETURN_ERRNO(ioctl(perf_fd, PERF_EVENT_IOC_SET_BPF, prog_fd) >= 0);
+  RETURN_ERRNO(ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0) >= 0);
+
+  return 0;
+}
