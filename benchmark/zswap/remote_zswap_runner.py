5c58af3eddfa18e1164448a40335f405ee5045a0
dariusgrassi <dariusgrassi@gmail.com>
2025-04-30 13:28:56 -0500

    tweak helper to allow changing workload distribution mix

--- benchmark/zswap/remote_zswap_runner.py
+++ benchmark/zswap/remote_zswap_runner.py
@@ -154,12 +154,16 @@ class RemoteZswapRunner:
 
     # provides option to setup either redis or mongodb benchmark
     # should run make install-yscb and copy over correct config
-    def setup_ycsb_experiment(self, benchmark: str, verbose=False):
+    def setup_ycsb_experiment(self, benchmark: str, distribution="uniform", verbose=False):
         if benchmark not in ['redis', 'mongodb']:
             print("Error! YCSB benchmark must be Redis or MongoDB")
             return -1
         self.execute_remote_command("make -C KernMLOps CONTAINER_CMD='make install-ycsb' docker", get_pty=True, verbose=verbose)
         self.execute_remote_command(f"cd KernMLOps/ && cp -v config/{benchmark}_no_collect.yaml overrides.yaml", verbose=verbose)
+        self.execute_remote_command(
+            f"""sed -i 's/request_distribution: ".*"/request_distribution: "{distribution}"/' KernMLOps/overrides.yaml""",
+            verbose=verbose
+        )
         return 0
 
     # runs a bunch of sysctl commands to aggressively shrink the Linux page
@@ -234,12 +238,11 @@ def main():
         remote_host='dwg@pc701.emulab.net',
         ssh_key='~/.ssh/cloudlab')
     runner.establish_connection()
-    runner.reset_connection()
-    # runner.reboot_machine()
     runner.clear_swap(verbose=True)
     runner.configure_zswap(parameter='enabled', value='0')
-    runner.setup_kernmlops(owner='dariusgrassi', branch='zswap-runner')
-    runner.setup_ycsb_experiment(benchmark='redis')
+    runner.setup_kernmlops(owner='dariusgrassi', branch='zswap-runner', verbose=True)
+    runner.reset_connection()
+    runner.setup_ycsb_experiment(benchmark='redis', distribution='zipfian', verbose=True)
     runner.shrink_page_cache()
     runner.run_mem_constrained_ycsb_experiment(benchmark='redis_uniform_nozswap')
     runner.find_and_parse_logfiles('redis_uniform*')
